1ea4979e4c8ddef5e859fafebeaf2e2012cb3c9a
Kelechi <kelechiowuama24@gmail.com>
2023-02-08 08:11:30 -0800

    all

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add_attribute(obj, a, v):
+
+    res = getattr(obj, "__doc__", None)
+
+    if res is None:
+
+        setattr(obj, a, v)
+
+    else:
+
+        raise TypeError("can't add new attribute")
