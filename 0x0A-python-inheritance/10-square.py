96d897685cf2083c360449b57074be68af8502e4
Kelechi <kelechiowuama24@gmail.com>
2023-02-08 11:19:16 -0800

    edited

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,6 +17,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
 
+        """ Constructor maping size to parent constructor"""
+
         super().integer_validator("size", size)
 
         super().__init__(size, size)
